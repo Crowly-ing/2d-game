8ff7430621fc85ca23588bcb61fb07572ee78a87
Allis Overlord <lina.leskova5891@gmail.com>
2021-04-30 16:48:28 +1000

    Background

--- index.js
+++ index.js
@@ -5,16 +5,17 @@ canvas.width = innerWidth
 canvas.height = innerHeight
 
 class Player {
-    constructor(x, y, radius, color) {
+    constructor(x, y, width, height, color) {
         this.x = x
         this.y = y
-        this.radius = radius
+        this.width = width
+        this.height = height
         this.color = color
     }
 
     draw() {
         c.beginPath()
-        c.drawImage(playerImage, this.x, this.y, canvas.width / 5, canvas.height / 2)
+        c.drawImage(playerImage, this.x, this.y, canvas.width / 10, canvas.height / 5)
     }
 }
 
@@ -29,13 +30,13 @@ class Bullet {
 
     draw() {
         c.beginPath()
-        c.drawImage(bulletImage, this.x, this.y, canvas.width / 5, canvas.height / 2)
+        c.drawImage(bulletImage, this.x, this.y, canvas.width / 50, canvas.height / 20)
     }
 
     update() {
         this.draw()
         this.x = this.x + this.velocity.x
-        this.y = this.y + this.velocity.y
+        this.y = this.y
     }
 }
 
@@ -50,7 +51,7 @@ class Enemy {
 
     draw() {
         c.beginPath()
-        c.drawImage(enemyImage, this.x, this.y, canvas.width / 5, canvas.height / 2)
+        c.drawImage(enemyImage, this.x, this.y, canvas.width / 10, canvas.height / 5)
     }
 
     update() {
@@ -61,19 +62,21 @@ class Enemy {
 }
 
 function loadImages() {
-    enemyImage = new Image();
-    playerImage = new Image();
-    bulletImage = new Image();
+    enemyImage = new Image()
+    playerImage = new Image()
+    bulletImage = new Image()
+    homeImage = new Image()
 
-    enemyImage.src = "img/enemy.png";
+    enemyImage.src = "img/enemy.png"
     playerImage.src = "img/player.png"
     bulletImage.src = "img/bullet.png"
+    homeImage.src = "img/home.png"
 }
 
 const x = canvas.width / 5
 const y = canvas.height / 2
 loadImages()
-const player = new Player(x, y, 40, 'red')
+const player = new Player(x, y, 10, canvas.height / 5 * 4, 'red')
 
 const bullets = []
 const enemies = []
@@ -82,7 +85,7 @@ function spawnEnemies() {
     setInterval(() => {
         const x = Math.random() * canvas.width / 5 + canvas.width
         const y = Math.random() * canvas.height
-        const radius = Math.random() * 20 + 10
+        const radius = 20
         const color = 'green'
         const angle = Math.atan2(
             canvas.height / 2 - y,
@@ -99,26 +102,38 @@ let animationId
 
 function animate() {
     animationId = requestAnimationFrame(animate)
-    c.clearRect(0, 0, canvas.width, canvas.height)
+    c.drawImage(homeImage, 0, 0, canvas.width, canvas.height)
+    c.fillStyle = 'rgba(0, 0, 0, 0.1)'
+    c.fillRect(0, 0, canvas.width, canvas.height)
     player.draw()
-    bullets.forEach((bullet) => {
+    bullets.forEach((bullet, index) => {
         bullet.update()
+
+        if (bullet.x + bullet.radius < 0 ||
+            bullet.x - bullet.radius > canvas.width ||
+            bullet.y + bullet.radius < 0 ||
+            bullet.y - bullet.radius > canvas.height) {
+            setTimeout(() => {
+                bullets.splice(index, 1)
+            }, 0)
+        }
     })
     enemies.forEach((enemy, index) => {
         enemy.update()
 
         const dist = Math.hypot(player.x - enemy.x,
             player.y - enemy.y)
-        if (dist - enemy.radius - player.radius < 1) {
+        if (dist - enemy.radius < 1) {
             cancelAnimationFrame(animationId)
         }
         bullets.forEach((bullet, bulletIndex) => {
             const dist = Math.hypot(bullet.x - enemy.x,
                 bullet.y - enemy.y)
             if (dist - enemy.radius - bullet.radius < 1) {
-                setTimeout(() => {}, 0)
-                enemies.splice(index, 1)
-                bullets.splice(bulletIndex, 1)
+                setTimeout(() => {
+                    enemies.splice(index, 1)
+                    bullets.splice(bulletIndex, 1)
+                }, 0)
             }
         })
     })
@@ -128,10 +143,10 @@ addEventListener('click', (event) => {
     const angle = Math.atan2(event.clientY - canvas.height / 2,
         event.clientX - canvas.width / 5)
     const velocity = {
-        x: Math.cos(angle),
-        y: Math.sin(angle)
+        x: Math.abs(Math.cos(angle)),
+        y: Math.abs(Math.sin(angle))
     }
-    bullets.push(new Bullet(canvas.width / 5, canvas.height / 2,
+    bullets.push(new Bullet(player.x, player.y,
         5, 'black', velocity))
 })
 
